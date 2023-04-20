4f948c9f8a6575b6772485bfe9d6fa690e5c2969
etallman1 <etallman@uw.edu>
2023-04-20 15:07:14 -0700

    I'm trying so hard to pull the last alpha for each person

--- ems_stuff/ems_analysis_attempt.R
+++ ems_stuff/ems_analysis_attempt.R
@@ -19,22 +19,20 @@ library("knitr")
 #want accurate answers out of total and last alpha for each 
 
 demographics_full_data <- read.csv("behavioral_data_raw/STAG consent and Demographics_April 14, 2023_17.58.csv") 
-demographics_full_data <- demographics_full_data
 demographics_full_data <- demographics_full_data[-1,] #removing row w/ column names
 demographics_full_data <- demographics_full_data[-1,] #removing row w/ import data
-demographics_full_data <- demographics_full_data[order(demographics_full_data$Subject,  
+demographics_full_data <- demographics_full_data[order(demographics_full_data$Subject..,  
                                                        decreasing = T),]
   
 
 #fixing demographic errors (birthdays listed as the date)
 col_index <- which(colnames(demographics_full_data) == "Date")
-row_index <- which(demographics_full_data$Subject == 7521)
+row_index <- which(demographics_full_data$Subject.. == 7521)
 
 demographics_full_data[row_index, col_index] <- "4/11/23"
 
 
-candy_slim_full_data <- read.csv("behavioral_data_raw/slim/9856_Candy_responses 2.csv") %>%
-  rename("Subject")
+candy_slim_full_data <- read.csv("behavioral_data_raw/slim/9856_Candy_responses 2.csv") 
 
 maps_slim_full_data <- read.csv("behavioral_data_raw/slim/9857_Maps_responses 2.csv")
 
@@ -43,17 +41,17 @@ maps_slim_full_data <- read.csv("behavioral_data_raw/slim/9857_Maps_responses 2.
 # demographics_full_data <- demographics_full_data %>%
 #   mutate(lang.classification2 = NA)
 
-for (i in 1:nrow(demographics_full_data)){
-  if (grepl("Early Bilingual", demographics_full_data$lang.classification[i])) {
-    demographics_full_data$lang.classification2[i] <- "Bilingual"
-  } else if (grepl("Late Bilingual", demographics_full_data$lang.classification[i])){
-    demographics_full_data$lang.classification2[i] <- "Bilingual"
-  } else if (grepl("Monolingual", demographics_full_data$lang.classification[i])){
-    demographics_full_data$lang.classification2[i] <- "Monolingual"
-  } else {
-    demographics_full_data$lang.classification2[i] <- "None of the above"
-  }
-}
+# for (i in 1:nrow(demographics_full_data)){
+#   if (grepl("Early Bilingual", demographics_full_data$lang.classification[i])) {
+#     demographics_full_data$lang.classification2[i] <- "Bilingual"
+#   } else if (grepl("Late Bilingual", demographics_full_data$lang.classification[i])){
+#     demographics_full_data$lang.classification2[i] <- "Bilingual"
+#   } else if (grepl("Monolingual", demographics_full_data$lang.classification[i])){
+#     demographics_full_data$lang.classification2[i] <- "Monolingual"
+#   } else {
+#     demographics_full_data$lang.classification2[i] <- "None of the above"
+#   }
+# }
 
 # #pulling bilingual 
 # bi_demographics <- demographics_full_data %>%
@@ -84,12 +82,93 @@ maps_first_subject_ids <- list()
 maps_first_subject_ids <- tibble(maps_first_subject_ids)
 
 candy_first_subject_ids <- list()
-for (i in seq(7501, demographics_full_data$Subject[1], by = 2)) {
+for (i in seq(7501, demographics_full_data$Subject..[1], by = 2)) {
   candy_first_subject_ids[[length(candy_first_subject_ids) + 1]] <- i
 }
 candy_first_subject_ids <- tibble(candy_first_subject_ids)
 
-#putting everything together
+#putting everything together===============================================
+#categorizing candy
+candy_slim_full_data <- candy_slim_full_data %>%
+  mutate(Type = "C", .before = "screen_name") %>%
+  rename("Subject.." = "screen_name") %>%
+  mutate(presented_first = NA, .before = "Subject..")
+#marking if first
+candy_slim_full_data$presented_first[candy_slim_full_data$Subject.. %in% 
+                                       candy_first_subject_ids$candy_first_subject_ids] <- TRUE
+candy_slim_full_data$presented_first[candy_slim_full_data$Subject.. %in% 
+                                       maps_first_subject_ids$maps_first_subject_ids] <- FALSE
+#marking if second
+candy_slim_full_data <- candy_slim_full_data %>%
+  mutate(presented_second = NA, .before = "Subject..")
+
+candy_slim_full_data$presented_second[candy_slim_full_data$Subject.. %in% 
+                                       candy_first_subject_ids$candy_first_subject_ids] <- FALSE
+candy_slim_full_data$presented_second[candy_slim_full_data$Subject.. %in% 
+                                       maps_first_subject_ids$maps_first_subject_ids] <- TRUE
+
+#categorizing maps
+maps_slim_full_data <- maps_slim_full_data %>%
+  mutate(Type = "M", .before = "screen_name") %>%
+  rename("Subject.." = "screen_name") %>%
+  mutate(presented_first = NA, .before = "Subject..")
+#marking if first
+maps_slim_full_data$presented_first[maps_slim_full_data$Subject.. %in% 
+                                       maps_first_subject_ids$maps_first_subject_ids] <- TRUE
+maps_slim_full_data$presented_first[maps_slim_full_data$Subject.. %in% 
+                                       candy_first_subject_ids$candy_first_subject_ids] <- FALSE
+#marking if second
+maps_slim_full_data <- maps_slim_full_data %>%
+  mutate(presented_second = NA, .before = "Subject..")
+
+maps_slim_full_data$presented_second[maps_slim_full_data$Subject.. %in% 
+                                        maps_first_subject_ids$maps_first_subject_ids] <- FALSE
+maps_slim_full_data$presented_second[maps_slim_full_data$Subject.. %in% 
+                                        candy_first_subject_ids$candy_first_subject_ids] <- TRUE
+
+master_slim <- rbind(maps_slim_full_data, candy_slim_full_data)
+master_slim <- master_slim[order(master_slim$Subject..,  
+                                                       decreasing = F),]
+
+#Getting highest sequence reached per subject per type=======================
+#maps
+maps_subject_list <- tibble(maps_slim_full_data$Subject..) %>%
+  distinct() %>%
+  rename("Subject.." = "maps_slim_full_data$Subject..")
+
+maps_last_sequence <- data_frame(matrix(nrow = nrow(maps_subject_list), ncol = 1))
+for (i in 1:nrow(maps_last_sequence)){
+maps_last_sequence[i, 1] <- max(subset(master_slim, Subject.. == maps_subject_list$Subject..[i] &
+                           Type == "M")$sequence_number)
+}
+maps_last_sequence <- maps_last_sequence %>%
+  mutate(Subject.. = maps_subject_list$Subject..) %>%
+  rename("last_sequence_number" = "matrix(nrow = nrow(maps_subject_list), ncol = 1)")
+
+#candy
+candy_subject_list <- tibble(candy_slim_full_data$Subject..) %>%
+  distinct() %>%
+  rename("Subject.." = "candy_slim_full_data$Subject..")
+
+candy_last_sequence <- data_frame(matrix(nrow = nrow(candy_subject_list), ncol = 1))
+for (i in 1:nrow(candy_last_sequence)){
+  candy_last_sequence[i, 1] <- max(subset(master_slim, Subject.. == candy_subject_list$Subject..[i] &
+                                           Type == "C")$sequence_number)
+}
+candy_last_sequence <- candy_last_sequence %>%
+  mutate(Subject.. = candy_subject_list$Subject..) %>%
+  rename("last_sequence_number" = "matrix(nrow = nrow(candy_subject_list), ncol = 1)")
+
+#figure out how to do this after combining the last sequences 
+maps_test <- maps_last_sequence %>%
+  left_join(master_slim, by = "Subject..") %>%
+  subset(sequence_number == last_sequence_number)
+
+
+
+
+
+
 
 
 
