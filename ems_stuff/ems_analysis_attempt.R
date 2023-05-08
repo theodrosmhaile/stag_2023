41101eb3a7e03dc6299fa7c61e14ae6937193238
etallman1 <etallman@uw.edu>
2023-05-08 12:51:59 -0700

    added TOD

--- ems_stuff/ems_analysis_attempt.R
+++ ems_stuff/ems_analysis_attempt.R
@@ -23,7 +23,6 @@ library("devtools")
 # The console will show a prompt asking whether all packages should be updated. Select option 1 (by typing 1 and then pressing enter), this will update all packages.
 #vignette("SlimStampeRVignette")
 
-#.xslx files were saved as .csv before importing 
 
 # function to capitalize the first letter of a word
 capitalize_word <- function(word) {
@@ -35,16 +34,15 @@ capitalize_word <- function(word) {
 #candy first and last, maps first and last
 #language 
 #facet ; box no -> candy and map box w x axis candy first or last, colors language, y accuracy 
-#want accurate answers out of total and last alpha for each 
 
 #lang classification df=================================================
 langs <- read_dataset("behavioral_data_raw/STAG_lang.csv") %>%
-  rename("screenName" = "id") #joining later by "screenName"
-langs <- langs[-nrow(langs)] #removed empty row at end
-langs <- langs[,-2] #removing gender column
+  rename("screenName" = "screen_name") #for consistency 
+langs <- langs[,-3] #removing gender column
+langs <- subset(langs, !duplicated(langs)) #removing dups
 
 #setting up candy data==================================================
-candy_raw_data <- read_dataset("behavioral_data_raw/slim/9856_Candy_responses 2.csv") %>%
+candy_raw_data <- read_dataset("behavioral_data_raw/slim/9856_Candy_responses.csv") %>%
   rename("presentationStartTime" = "presentation_start_time")
 
 # get the column names of the data
@@ -69,12 +67,14 @@ new_col_names <- sapply(col_words_modified, function(x) {
 # set the new column names
 colnames(candy_raw_data) <- new_col_names
 
+
 #repeat for maps========================================================
-maps_raw_data <- read_dataset("behavioral_data_raw/slim/9857_Maps_responses 2.csv") %>%
+maps_raw_data <- read_dataset("behavioral_data_raw/slim/9857_Maps_responses.csv") %>%
   rename("presentationStartTime" = "presentation_start_time")
 
 # set the new column names
 colnames(maps_raw_data) <- new_col_names
+
 
 #removing Teddy==============================================
 candy_raw_data <- candy_raw_data %>%
@@ -82,13 +82,47 @@ candy_raw_data <- candy_raw_data %>%
 maps_raw_data <- maps_raw_data %>%
   filter(screenName != "TeddyHaile")
 
-#make columns for lesson + repitition===============================================
+#make columns for lesson + repetition + time of day=============================
 candy_raw_data <- candy_raw_data %>%
   mutate(lessonTitle = "C", .before = "screenName") %>%
   calculate_repetition()
 maps_raw_data <- maps_raw_data %>%
   mutate(lessonTitle = "M", .before = "screenName") %>%
   calculate_repetition()
+
+candy_raw_data <- candy_raw_data %>%
+  mutate(candyBeforeNoon = createTime)
+#fix time
+candy_raw_data$candyBeforeNoon <- as.POSIXct(candy_raw_data$candyBeforeNoon) 
+candy_raw_data$candyBeforeNoon <- as.POSIXct(as.numeric(candy_raw_data$candyBeforeNoon) - 2*60*60, 
+                                       origin="1970-01-01") 
+#pull time only
+candy_raw_data$candyBeforeNoon <- format(as.POSIXct(candy_raw_data$candyBeforeNoon), format = "%H:%M:%S")
+
+for (i in 1:nrow(candy_raw_data)){
+  if(candy_raw_data$candyBeforeNoon[i] > 12){
+    candy_raw_data$candyBeforeNoon[i] <- FALSE
+  } else {
+    candy_raw_data$candyBeforeNoon[i] <- TRUE
+  }
+}
+#repeat for maps
+maps_raw_data <- maps_raw_data %>%
+  mutate(mapsBeforeNoon = createTime)
+#fix time
+maps_raw_data$mapsBeforeNoon <- as.POSIXct(maps_raw_data$mapsBeforeNoon) 
+maps_raw_data$mapsBeforeNoon <- as.POSIXct(as.numeric(maps_raw_data$mapsBeforeNoon) - 2*60*60, 
+                                        origin="1970-01-01") 
+#pull time only
+maps_raw_data$mapsBeforeNoon <- format(as.POSIXct(maps_raw_data$mapsBeforeNoon), format = "%H:%M:%S")
+
+for (i in 1:nrow(maps_raw_data)){
+  if(maps_raw_data$mapsBeforeNoon[i] > 12){
+    maps_raw_data$mapsBeforeNoon[i] <- FALSE
+  } else {
+    maps_raw_data$mapsBeforeNoon[i] <- TRUE
+  }
+}
 
 #getting data for first/last version===========================================
 #making lists for which subjects did what first
@@ -134,25 +168,25 @@ maps_raw_data$candy_first[maps_raw_data$screenName %in%
 #adding languages and averages====================================================
 langs$screenName <- as.character(langs$screenName)
 #candy
-candy_raw_data <- left_join(candy_raw_data, langs, by = "screenName")
+candy_raw_data <- full_join(candy_raw_data, langs, by = "screenName")
 candy_avg <- candy_raw_data %>%
   group_by(screenName, maps_first, candy_first, language) %>%
   summarize(candy_mean_rof = mean(alpha))
 accuracy_candy <- candy_raw_data %>%
   group_by(screenName)%>%
   summarize(candy_mean_accuracy = mean(correct))
-candy_avg <- left_join(candy_avg, accuracy_candy)
+candy_avg <- full_join(candy_avg, accuracy_candy)
 #maps
-maps_raw_data <- left_join(maps_raw_data, langs, by = "screenName")
+maps_raw_data <- full_join(maps_raw_data, langs, by = "screenName")
 maps_avg <- maps_raw_data %>%
   group_by(screenName, maps_first, candy_first, language) %>%
   summarize(maps_mean_rof = mean(alpha))
 accuracy_maps <- maps_raw_data %>%
   group_by(screenName)%>%
   summarize(maps_mean_accuracy = mean(correct))
-maps_avg <- left_join(maps_avg, accuracy_maps)
+maps_avg <- full_join(maps_avg, accuracy_maps)
 #combining data=====================================
-master_data <- left_join(maps_avg, candy_avg) %>%
+master_data <- full_join(maps_avg, candy_avg) %>%
   group_by(screenName, maps_first, language)
 # master_data$maps_first <- ifelse(master_data$maps_first == FALSE, "Candy First",
 #                                  master_data$maps_first)
@@ -162,15 +196,22 @@ master_data <- left_join(maps_avg, candy_avg) %>%
 # master_averages <- master_data %>%
 #   group_by()
 
+#adding columns for time they *started* the task
+map_time <- subset(maps_raw_data, !duplicated(screenName)) %>%
+  select(screenName, mapsBeforeNoon)
+map_time <- map_time[4:5]
+candy_time <- subset(candy_raw_data, !duplicated(screenName)) %>%
+  select(screenName, candyBeforeNoon)
+candy_time <- candy_time[4:5]
+master_data <- left_join(master_data, map_time)
+master_data <- left_join(master_data, candy_time)
 
-avg_plot <- plot_ly(master_data, x = ~language, color = ~maps_first, hoverinfo = "none") %>%
+dups <- master_data[duplicated(master_data$screenName), ]
+#what is this????????????
+
+candy_avg_plot <- plot_ly(master_data, x = ~language, color = ~maps_first, hoverinfo = "none") %>%
   add_trace(y = ~mean(maps_mean_rof), type = "bar") %>%
-  add_trace(y = ~mean(candy_mean_rof), type = "bar", showlegend = F) %>%
-  layout(barmode = "group") #%>%
-  # add_trace(y = ~maps_mean_rof, type = "box") %>%
-  # add_trace(y = ~candy_mean_rof, type = "box", showlegend = F) 
-  
-avg_plot
+  layout(barmode = "group")
 
 
 
